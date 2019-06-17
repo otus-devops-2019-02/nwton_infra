01329878a4476707361538b865f5647b2b9d26e7
Sergey Slabnov <sergey.slabnov@gmail.com>
2019-06-18 01:58:30 +0300

    Play HW9 step5

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  # Описание переменной
+  description = "Region"
+
+  # Значение по умолчанию
+  default = "europe-west1"
+}
