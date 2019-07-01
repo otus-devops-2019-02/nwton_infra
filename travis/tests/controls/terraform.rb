df495ceb309e4eb31b805babb2929246cdd79202
Sergey Slabnov <sergey.slabnov@gmail.com>
2019-07-01 23:38:22 +0300

    Change terraform copy-paste to array and add tflint

--- travis/tests/controls/terraform.rb
+++ travis/tests/controls/terraform.rb
@@ -6,31 +6,24 @@ control 'terraform' do
   impact 1
   title 'Run terraform validate for stage & prod'
 
-  describe file('terraform/stage/terraform.tfvars.example') do
-    it { should exist }
-    its('content') { should match(%r{\n\Z}) }
-  end
+  environments = ['/', 'stage/', 'prod/']
 
-  describe file('terraform/prod/terraform.tfvars.example') do
-    it { should exist }
-    its('content') { should match(%r{\n\Z}) }
-  end
+  environments.each do |fname|
+    describe file("terraform/#{fname}/terraform.tfvars.example") do
+      it { should exist }
+      its('content') { should match(%r{\n\Z}) }
+    end
 
-  describe command('cd terraform && terraform init -backend=false && terraform validate -var-file=terraform.tfvars.example') do
-    its('stdout') { should match "Terraform has been successfully initialized!" }
-    its('stderr') { should eq '' }
-    its('exit_status') { should eq 0 }
-  end
+    describe command('cd terraform/#{fname} && terraform init -backend=false && terraform validate -var-file=terraform.tfvars.example') do
+      its('stdout') { should match "Terraform has been successfully initialized!" }
+      its('stderr') { should eq '' }
+      its('exit_status') { should eq 0 }
+    end
 
-  describe command('cd terraform/prod && terraform init -backend=false && terraform validate -var-file=terraform.tfvars.example') do
-    its('stdout') { should match "Terraform has been successfully initialized!" }
-    its('stderr') { should eq '' }
-    its('exit_status') { should eq 0 }
-  end
-
-  describe command('cd terraform/stage && terraform init -backend=false && terraform validate -var-file=terraform.tfvars.example') do
-    its('stdout') { should match "Terraform has been successfully initialized!" }
-    its('stderr') { should eq '' }
-    its('exit_status') { should eq 0 }
+    describe command("cd terraform/#{fname} && tflint --var-file=terraform.tfvars.example --deep -q") do
+      its('stdout') { should eq "" }
+      its('stderr') { should eq "" }
+      its('exit_status') { should eq 0 }
+    end
   end
 end
